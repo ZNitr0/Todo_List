9bbead52d851da167421a768cfc22ef2c2b1691f
ZNitr0 <levidonascimento2410@gmail.com>
2025-08-12 21:11:25 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { todo } from 'src/model/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: todo[] = [] ;
+  public title: string = ('Lista de Tarefas');
+
+  constructor() {
+    this.todos.push( {id: 1, title:'estudar angular', done: false} );
+     this.todos.push({id: 2, title:'ir a cademia ', done: false})
+      this.todos.push({id: 3, title:'tomar 5 litros de água', done: true}) 
+  }
+
+  remover( todo : todo ){
+      const index = this.todos.indexOf(todo);
+      if(index !== -1){
+        this.todos.splice(index, 1);
+      }
+  }
+
+  markAsDone( todo : todo ){
+    todo.done = true;
+  }
+
+  markAsUnDone( todo : todo ){
+    todo.done = false;
+  }
+
 }
+
---
+++ src/model/todo.model.ts
@@ -0,0 +1,9 @@
+
+export class todo {
+    constructor(
+        public id: number,
+        public title: string,
+        public done: boolean
+    ) {
+    }
+}
